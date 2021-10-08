2d1856de88d412d8990c7dccbde83fe71f037bb1
yohanesrioirsan <82473445+yohanesrioirsan@users.noreply.github.com>
2021-10-08 19:30:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ buttons.forEach(e => {
         output.value = translate(e.id);
     })
 });
+
+function copy_text() {
+  var copyText = document.getElementById("output");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
